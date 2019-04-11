83f0754361ffa4a335d9a8643736ccd9f44c571a
hao <hao@hao-PC>
2019-04-11 17:06:08 +0800

    返回访问者公网IP

--- src/com/hao/ip/IPServlet.java
+++ src/com/hao/ip/IPServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * 返回访问者IP - IPServlet
  */
-@WebServlet("/")
+@WebServlet("/ip")
 public class IPServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
