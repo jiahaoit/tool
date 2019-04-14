b528c8e1179c21da7d05f39785e0c601880083dc
hao <hao@hao-PC>
2019-04-14 10:48:55 +0800

    初步接触Junit

---
+++ src/com/hao/junit/GeneralMain.java
@@ -0,0 +1,13 @@
+package com.hao.junit;
+
+public class GeneralMain {
+
+	public static void main(String[] args) {
+		int result = sum1(1, 2);
+		System.out.println(result);
+
+	}
+	public static int sum1(int i, int j) {
+        return i+j;
+    }
+}
